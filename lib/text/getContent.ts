dafda4acf32258eab1a1be41a9f9ae93d0f56572
Seojeonguk <uk7880@naver.com>
2021-12-17 13:48:17 +0900

    Fix the file name
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- lib/text/getContent.ts
+++ lib/text/getContent.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 
 const getContent = async () => {
-  const textFile = readFileSync(path.join(__dirname,"../../../public/correctProblem.txt"),"utf-8");
+  const textFile = readFileSync(path.join(__dirname,"../../../public/solvedProblem.txt"),"utf-8");
   const solvedProblems = textFile.split(' ');
   return solvedProblems;
 }
