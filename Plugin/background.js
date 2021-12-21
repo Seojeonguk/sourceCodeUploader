5649c0537fdf59cc6952f124ee10bc40f79a702c
Seojeonguk <uk7880@naver.com>
2021-12-21 15:59:20 +0900

    Create an upload button
    This will create an upload button for the correct problem result
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- Plugin/background.js
+++ Plugin/background.js
@@ -6,8 +6,18 @@ chrome.action.onClicked.addListener((tab) => {
 });
 
 async function dataload() {
-    document.querySelectorAll("tbody tr").forEach((item) => {
-        if (item.childNodes[3].firstChild.innerText === "맞았습니다!!") {
+    document.querySelectorAll("tbody tr").forEach((tr) => {
+        if (tr.childNodes[3].firstChild.innerText === "맞았습니다!!") {
+            let newTd = document.createElement("td");
+            let newBtn = doucment.createElement("button");
+            newBtn.innerText = "Upload";
+            newBtn.setAttribute("submissionNum", tr.firstChild.innerText);
+            newBtn.setAttribute("problemNum", tr.childNodes[2].lastChild.innerText);
+            newBtn.onclick = function() {
+              // todo
+            }
+            newTd.appendChild(newBtn);
+            tr.appendChild(newTd);
         }
     });
 }
