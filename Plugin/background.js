c5e1cbef289ac4470b676bc34771ec437e4577d2
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2021-12-27 09:19:02 +0900

    Get the accepted problem of source code (#8)
    This will find the source code of the accepted problem.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- Plugin/background.js
+++ Plugin/background.js
@@ -13,9 +13,31 @@ async function dataload() {
             newBtn.innerText = "Upload";
             newBtn.setAttribute("submissionNum", tr.firstChild.innerText);
             newBtn.setAttribute("problemNum", tr.childNodes[2].lastChild.innerText);
-            newBtn.onclick = function() {
-              // todo
-            }
+            newBtn.onclick = function () {
+                var sourcecode;
+
+                var sourcecodeConn = new XMLHttpRequest();
+                var url = `https://www.acmicpc.net/source/${this.getAttribute("submissionNum")}`;
+
+                sourcecodeConn.open("GET", url, false);
+                sourcecodeConn.onreadystatechange = function () {
+                    if (sourcecodeConn.readyState == 4 && sourcecodeConn.status == 200) {
+                      let responseText = sourcecodeConn.responseText;
+
+                      let openTextarea = new RegExp("(<textarea[^>]*>)", "g");
+                      let closeTextarea = new RegExp("(</textarea>)", "g");
+                      
+                      sourcecode = responseText.split(openTextarea)[2].split(closeTextarea)[0];
+                      sourcecode = sourcecode.replace(/&lt;/gi, "<");
+                      sourcecode = sourcecode.replace(/&gt;/gi, ">");
+                      sourcecode = sourcecode.replace(/&quot;/gi, '"');
+                      sourcecode = sourcecode.replace(/&amp;/gi, "&");
+                    }
+                };
+                sourcecodeConn.send();
+                
+                // TODO Add more
+            };
             newTd.appendChild(newBtn);
             tr.appendChild(newTd);
         }
