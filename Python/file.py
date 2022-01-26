f658097a2d0aad41d3c2fc4a47f562c2d51f0af5
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-01-26 16:19:16 +0900

    Make sure it belongs to the problem I solved (#18)
    This will check if the problem to upload belongs to the problem I solved.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- Python/file.py
+++ Python/file.py
@@ -1,3 +1,19 @@
+import os
+
+
 class File:
     def __init__(self):
         pass
+
+    def checkproblem(self, problemNum):
+        existing = True
+
+        if os.path.exists("./problem.txt"):
+            with open("./problem.txt", "r") as file:
+                problems = file.readline().split(' ')
+                file.close()
+
+        if problemNum in problems:
+            existing = False
+
+        return existing
