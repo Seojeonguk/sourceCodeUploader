40d806556c5ed0ee4b89c9beaf14977bc96ed263
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-01-26 16:49:12 +0900

    Get problem information (#20)
    This will bring information about tags, titles, and levels for the problem.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- Python/solvedac.py
+++ Python/solvedac.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Solvedac:
     levels = ['Unrated']
     level = ['브론즈', '실버', '골드', '플래티넘', '다이아몬드', '루비']
@@ -9,3 +12,16 @@ class Solvedac:
         for Lv in self.level:
             for idx in range(5, 0, -1):
                 self.levels.append(Lv+str(idx))
+
+    def problemInfo(self, problemNum):
+        querystring = {"problemId": problemNum}
+        response = requests.request(
+            "GET", self.url, headers=self.headers, params=querystring)
+        title = response.json()['titleKo']
+        tags = []
+        level = self.levels[response.json()['level']]
+
+        for item in response.json()['tags']:
+            tags.append(item['displayNames'][1]['name'])
+
+        return title, level, tags
