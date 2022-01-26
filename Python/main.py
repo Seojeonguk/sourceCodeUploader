80c5b3379f6c14d6441486080848d767d4913123
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-01-26 13:24:09 +0900

    This will detect changes in the system (#14)
    This will detect system changes in the route you set.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

---
+++ Python/main.py
@@ -0,0 +1,45 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Target:
+    watchDir = "../"
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchDir, recursive=True)
+        self.observer.start()
+
+        try:
+            while True:
+                time.sleep(10)
+        except:
+            self.observer.stop()
+            print("Error")
+            self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def on_moved(self, event):
+        pass
+
+    def on_created(self, event):  # 파일, 디렉터리가 생성되면 실행
+        pass
+
+    def on_deleted(self, event):  # 파일, 디렉터리가 삭제되면 실행
+        pass
+
+    def on_modified(self, event):  # 파일, 디렉터리가 수정되면 실행
+        pass
+
+
+if __name__ == "__main__":
+    w = Target()
+    w.run()
