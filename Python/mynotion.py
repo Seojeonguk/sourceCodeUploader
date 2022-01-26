94542454ff7e605f3af10aa3a01ed2499dc92116
Seojeonguk <uk7880@naver.com>
2022-01-26 17:08:20 +0900

    Add a page to Notion
    This will add a new page to Notion.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- Python/mynotion.py
+++ Python/mynotion.py
@@ -1,6 +1,7 @@
 from notion.client import NotionClient
 from dotenv import load_dotenv
 import os
+import datetime
 
 
 class Notion:
@@ -8,3 +9,16 @@ class Notion:
         load_dotenv()
         client = NotionClient(token_v2=os.environ.get("token"))
         self.page = client.get_collection_view(os.environ.get("url"))
+
+    def addpage(self, problemNum, title, level, tags, sourcecode):
+        newPage = self.page.collection.add_row()
+        newPage.set_property('title', title)
+        newPage.set_property('문제정보', ['BOJ', level, problemNum])
+        newPage.set_property('태그', tags)
+        newPage.set_property(
+            'URL', f'https://www.acmicpc.net/problem/{problemNum}')
+        newPage.set_property('Date', datetime.date.today())
+        language = "C++"
+        if "bits/stdc++.h" not in sourcecode:
+            language = "Java"
+        newPage.set_property('풀이', language)
