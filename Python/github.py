9dc51afe5a47096a0f27270871860ec4dd487ce7
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-02-04 16:03:12 +0900

    Fix github url (#56)
    This will fix the folder url of github.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- Python/github.py
+++ Python/github.py
@@ -11,6 +11,7 @@ class Github:
         self.githubToken = os.environ.get("github_token")
         self.githubOwner = os.environ.get("github_owner")
         self.githubRepo = os.environ.get("github_repo")
+        self.githubFolderPath = os.environ.get("github_folder_path")
         self.githubApiUrl = "https://api.github.com"
 
         self.headers = {
@@ -25,7 +26,7 @@ class Github:
         elif language == 'Python':
             ext = "py"
 
-        path = 'BOJ/%s.%s' % (problemNum, ext)
+        path = '%s%s.%s' % (self.githubFolderPath, problemNum, ext)
         url = '%s/repos/%s/%s/contents/%s' % (
             self.githubApiUrl, self.githubOwner, self.githubRepo, path)
 
@@ -37,6 +38,6 @@ class Github:
         res = requests.put(url=url, headers=self.headers, data=json.dumps(da))
 
         if res.status_code == 201 or res.status_code == 200:
-            print('%s를 정상적으로 업로드 하였습니다.' % (problemNum))
+            print('%s를 정상적으로 Github에 업로드 하였습니다.' % (problemNum))
         else:
-            print('%s를 업로드 하는 도중 오류가 발생하였습니다.' % (problemNum))
+            print('%s를 Github에 업로드 하는 도중 오류가 발생하였습니다.' % (problemNum))
