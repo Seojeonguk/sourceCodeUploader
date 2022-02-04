b6dfa3a1bcb4358dbfa14dfbeea86d1108ade35f
Seojeonguk <uk7880@naver.com>
2022-02-04 14:48:11 +0900

    Make initial settings related to github
    This will do the initial setting related to github.
    
    Signed-off-by: Seojeonguk <uk7880@naver.com>

--- Python/github.py
+++ Python/github.py
@@ -1,4 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+
 class Github:
     def __init__(self):
-        pass
-        # Todo Add more
+        load_dotenv()
+        self.githubToken = os.environ.get("github_token")
+        self.githubOwner = os.environ.get("github_owner")
+        self.githubRepo = os.environ.get("github_repo")
+        self.githubApiUrl = "https://api.github.com"
+
+        self.headers = {
+            'Authorization': 'Bearer %s' % (self.githubToken),
+            'Accept': 'application/vnd.github.v3+json'
+        }
