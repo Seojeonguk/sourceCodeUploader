d4d1735510391c152d6b8cab63c847691bbbc4e6
Seojeonguk <junguk7880@naver.com>
2022-12-23 03:31:57 +0900

    Add github upload button
    This will add github upload button.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- Plugin/boj-content-script.js
+++ Plugin/boj-content-script.js
@@ -2,3 +2,44 @@
 // 1. notion upload button
 // 2. github upload button
 // 3. all upload button
+$(function () {
+  const btnWrap = createBtnWrap();
+  createGithubBtn(btnWrap, 0);
+});
+
+function createBtnWrap() {
+  const addingElement = $(".CodeMirror");
+
+  const btnWrap = document.createElement("div");
+
+  btnWrap.style.position = "absolute";
+  btnWrap.style.top = 0;
+  btnWrap.style.right = 0;
+  btnWrap.style.zIndex = 9999;
+  btnWrap.style.padding = "10px";
+
+  addingElement.append(btnWrap);
+
+  return btnWrap;
+}
+
+function createGithubBtn(btnWrap, isDarkmode) {
+  const githubBtn = document.createElement("a");
+  githubBtn.href = "#";
+  githubBtn.style.cursor = "pointer";
+  githubBtn.onclick = function (e) {
+    // To do more..
+    // Request server
+  };
+
+  var githubURL = "icon/githubIcon.png";
+  if (isDarkmode) {
+    githubURL = "icon/githubDarkIcon.png";
+  }
+
+  githubBtn.innerHTML = `<img src="${chrome.runtime.getURL(
+    githubURL
+  )}" alt="github" style="width:32px; height:32px;" />`;
+
+  btnWrap.append(githubBtn);
+}
