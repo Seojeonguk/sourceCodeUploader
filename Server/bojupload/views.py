c6d6d530360ee0f1669344f47e09abfa7b68bbed
Seojeonguk <junguk7880@naver.com>
2022-12-26 01:48:01 +0900

    Add upload github
    This will add upload github.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- Server/bojupload/views.py
+++ Server/bojupload/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['POST'])
+def githubUpload(request):
+    return Response("success",status=status.HTTP_200_OK)
