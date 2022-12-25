ff0e28d34bf0628182570ba3925d1238ec94102f
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-12-26 02:27:29 +0900

    Set cors headers (#80)
    This will only allow the cors setting for BOJ.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- Server/autoupload/settings.py
+++ Server/autoupload/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'bojupload'
+    'bojupload',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://www.acmicpc.net"
+]
+
+CORS_ALLOW_METHODS = [
+    "POST"
 ]
 
 ROOT_URLCONF = 'autoupload.urls'
@@ -123,3 +133,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
