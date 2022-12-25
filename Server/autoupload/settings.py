99c1efe42685adc5e9df4792ee804cf54ba19a15
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-12-26 00:24:26 +0900

    Change the server settings (#75)
    This will update the settings on the server.
    
    Signed-off-by: Seojeonguk junguk7880@naver.com

--- Server/autoupload/settings.py
+++ Server/autoupload/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)mbt5u5a7*m17o0r9*4s_yhecyserhai6c=w3%e^-cq7&pzr0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
