e7a5c989fe4382a9aa6bfb8d1cf461fda51285f1
Seojeonguk <junguk7880@naver.com>
2022-12-29 01:15:10 +0900

    Arrange functions
    This will arrange functions by name.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- Server/bojupload/helpers.py
+++ Server/bojupload/helpers.py
@@ -1,40 +1,18 @@
 from . import constants
 
-import requests
-import json
 import base64
+import json
+import requests
 
-def verifyGithubInfo(requestData):
-    if isEmpty(requestData.get('githubToken')):
-        raise Exception('Does not exist github token!')
-    
-    if isEmpty(requestData.get('githubUserName')):
-        raise Exception('Does not exist github username!')
-
-    if isEmpty(requestData.get('githubRepo')):
-        raise Exception('Does not exist github repository!')
-
-    if isEmpty(requestData.get('githubFolderPath')):
-        raise Exception('Does not exist github folder path!')
-
-def verifyProblemInfo(requestData):
-    if isEmpty(requestData.get('problemId')):
-        raise Exception('Does not exist problem ID!')
-
-    if isEmpty(requestData.get('mime')):
-        raise Exception('Does not exist mime!')
-
-    if isEmpty(requestData.get('sourcecode')):
-        raise Exception('Does not exist code!')
-
-def requestsolvedac(problemId):
-    url = '%s/%s' % (constants.SOLVEDAC_BASE_URL, constants.GETTING_PROBLEM_WITH_ID_URL)
-    headers = {"Content-Type" : "application/json"}
-    params = {"problemId" : problemId}
-
-    response = requests.request("GET", url, headers = headers, params = params)
-
-    return response.json()
+def getEtx(ext):
+    extension = {
+        'text/x-csrc' : constants.FILE_EXTENSION_C,
+        'text/x-c++src' : constants.FILE_EXTENSION_CPLUSCPLUS,
+        'text/x-java' : constants.FILE_EXTENSION_JAVA,
+        'text/x-python' : constants.FILE_EXTENSION_PYTHON,
+        'text/plain' : constants.FILE_EXTENSION_TEXT
+    }
+    return extension.get(ext)
 
 def getGithubInfo(requestData):
     githubInfo = {
@@ -69,13 +47,12 @@ def github(problemInfo, githubInfo):
         'Accept': 'application/vnd.github.v3+json'
     }
 
-    path = '%s%s.%s' % (githubInfo.get('folderPath'),
-                        problemInfo['problemId'], problemInfo['ext'])
+    path = '%s%s.%s' % (githubInfo.get('folderPath'), problemInfo.get('problemId'), problemInfo.get('ext'))
     url = '%s/repos/%s/%s/contents/%s' % (constants.GITHUB_BASE_URL,
                                           githubInfo.get('userName'), githubInfo.get('repo'), path)
 
     requestData = {
-        "message": problemInfo['title'],
+        "message": problemInfo.get('title'),
         "content": base64.b64encode(problemInfo.get('sourcecode').encode('ascii')).decode('utf8')
     }
 
@@ -83,19 +60,43 @@ def github(problemInfo, githubInfo):
 
     if res.status_code==422:
         return res.json().get('message')
+
     if res.status_code==201 or res.status_code==200:
         return res.json()['content']['html_url']
-
-def getEtx(ext):
-    extension = {
-        'text/x-csrc' : constants.FILE_EXTENSION_C,
-        'text/x-c++src' : constants.FILE_EXTENSION_CPLUSCPLUS,
-        'text/x-java' : constants.FILE_EXTENSION_JAVA,
-        'text/x-python' : constants.FILE_EXTENSION_PYTHON,
-        'text/plain' : constants.FILE_EXTENSION_TEXT
-    }
-    return extension.get(ext)
 
 def isEmpty(o):
     if o == None or o == '':
         return True
+    return False
+
+def requestsolvedac(problemId):
+    url = '%s/%s' % (constants.SOLVEDAC_BASE_URL, constants.GETTING_PROBLEM_WITH_ID_URL)
+    headers = {"Content-Type" : "application/json"}
+    params = {"problemId" : problemId}
+
+    response = requests.request("GET", url, headers = headers, params = params)
+
+    return response.json()
+
+def verifyGithubInfo(requestData):
+    if isEmpty(requestData.get('githubToken')):
+        raise Exception('Does not exist github token!')
+    
+    if isEmpty(requestData.get('githubUserName')):
+        raise Exception('Does not exist github username!')
+
+    if isEmpty(requestData.get('githubRepo')):
+        raise Exception('Does not exist github repository!')
+
+    if isEmpty(requestData.get('githubFolderPath')):
+        raise Exception('Does not exist github folder path!')
+
+def verifyProblemInfo(requestData):
+    if isEmpty(requestData.get('problemId')):
+        raise Exception('Does not exist problem ID!')
+
+    if isEmpty(requestData.get('mime')):
+        raise Exception('Does not exist mime!')
+
+    if isEmpty(requestData.get('sourcecode')):
+        raise Exception('Does not exist code!')
