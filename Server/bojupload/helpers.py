1d8d0dbe828b81138133abd5aa9ec8bb3284d783
Seojeonguk <44386047+Seojeonguk@users.noreply.github.com>
2022-12-29 02:45:02 +0900

    Add upload github (#78)
    This will add upload github.
    
    Signed-off-by: Seojeonguk junguk7880@naver.com

---
+++ Server/bojupload/helpers.py
@@ -0,0 +1,128 @@
+from . import constants
+
+import base64
+import json
+import requests
+
+
+def getEtx(ext):
+    extension = {
+        'text/x-csrc': constants.FILE_EXTENSION_C,
+        'text/x-c++src': constants.FILE_EXTENSION_CPLUSCPLUS,
+        'text/x-java': constants.FILE_EXTENSION_JAVA,
+        'text/x-python': constants.FILE_EXTENSION_PYTHON,
+        'text/plain': constants.FILE_EXTENSION_TEXT
+    }
+    return extension.get(ext)
+
+
+def getGithubInfo(requestData):
+    githubInfo = {
+        'token': requestData.get('githubToken'),
+        'userName': requestData.get('githubUserName'),
+        'repo': requestData.get('githubRepo'),
+        'folderPath': requestData.get('githubFolderPath')
+    }
+
+    return githubInfo
+
+
+def getProblemInfo(requestData):
+    problemId = requestData.get('problemId')
+    mime = requestData.get('mime')
+
+    problemFullInfo = requestsolvedac(problemId)
+
+    problemInfo = {
+        'problemId': problemId,
+        'title': problemFullInfo.get('titleKo'),
+        'sourcecode': requestData.get('sourcecode'),
+        'ext': getEtx(mime)
+    }
+
+    return problemInfo
+
+
+def getSHA(problemInfo, githubInfo):
+    headers = {
+        'Authorization': 'Bearer %s' % (githubInfo.get('token')),
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
+    path = '%s%s.%s' % (githubInfo.get('folderPath'),
+                        problemInfo.get('problemId'), problemInfo.get('ext'))
+    url = '%s/repos/%s/%s/contents/%s' % (constants.GITHUB_BASE_URL,
+                                          githubInfo.get('userName'), githubInfo.get('repo'), path)
+
+    res = requests.get(url=url, headers=headers)
+
+    return res.json().get('sha')
+
+
+def github(problemInfo, githubInfo, sha):
+    headers = {
+        'Authorization': 'Bearer %s' % (githubInfo.get('token')),
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
+    path = '%s%s.%s' % (githubInfo.get('folderPath'),
+                        problemInfo.get('problemId'), problemInfo.get('ext'))
+    url = '%s/repos/%s/%s/contents/%s' % (constants.GITHUB_BASE_URL,
+                                          githubInfo.get('userName'), githubInfo.get('repo'), path)
+
+    requestData = {
+        "message": problemInfo.get('title'),
+        "content": base64.b64encode(problemInfo.get('sourcecode').encode('ascii')).decode('utf8')
+    }
+    if not isEmpty(sha):
+        requestData['sha'] = sha
+
+    res = requests.put(url=url, headers=headers, data=json.dumps(requestData))
+
+    if res.status_code == 422:
+        return res.json().get('message')
+
+    if res.status_code == 201 or res.status_code == 200:
+        return res.json()['content']['html_url']
+
+
+def isEmpty(o):
+    if o == None or o == '':
+        return True
+    return False
+
+
+def requestsolvedac(problemId):
+    url = '%s/%s' % (constants.SOLVEDAC_BASE_URL,
+                     constants.GETTING_PROBLEM_WITH_ID_URL)
+    headers = {"Content-Type": "application/json"}
+    params = {"problemId": problemId}
+
+    response = requests.request("GET", url, headers=headers, params=params)
+
+    return response.json()
+
+
+def verifyGithubInfo(requestData):
+    if isEmpty(requestData.get('githubToken')):
+        raise Exception('Does not exist github token!')
+
+    if isEmpty(requestData.get('githubUserName')):
+        raise Exception('Does not exist github username!')
+
+    if isEmpty(requestData.get('githubRepo')):
+        raise Exception('Does not exist github repository!')
+
+    if isEmpty(requestData.get('githubFolderPath')):
+        raise Exception('Does not exist github folder path!')
+
+
+def verifyProblemInfo(requestData):
+    if isEmpty(requestData.get('problemId')):
+        raise Exception('Does not exist problem ID!')
+
+    if isEmpty(requestData.get('mime')):
+        raise Exception('Does not exist mime!')
+
+    if isEmpty(requestData.get('sourcecode')):
+        raise Exception('Does not exist code!')
--- Server/bojupload/views.py
+++ Server/bojupload/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import helpers
+
+
+@api_view(['POST'])
+def githubUpload(request):
+    try:
+        helpers.verifyGithubInfo(request.data)
+        helpers.verifyProblemInfo(request.data)
+        githubInfo = helpers.getGithubInfo(request.data)
+        problemInfo = helpers.getProblemInfo(request.data)
+        sha = helpers.getSHA(problemInfo, githubInfo)
+        response = helpers.github(problemInfo, githubInfo, sha)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+    return Response(response, status=status.HTTP_201_CREATED)
