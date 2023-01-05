8f5040d2c8ab43194d58373d2e35f0692336d5e3
Seojeonguk <junguk7880@naver.com>
2023-01-05 16:12:06 +0900

    Check for dark mode depending on the theme
    This will provide white icons according to the theme.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- Plugin/boj-content-script.js
+++ Plugin/boj-content-script.js
@@ -4,7 +4,7 @@
 // 3. all upload button
 $(function () {
   const btnWrap = createBtnWrap();
-  createGithubBtn(btnWrap, 0);
+  createGithubBtn(btnWrap, theme);
 });
 
 function createBtnWrap() {
@@ -23,7 +23,7 @@ function createBtnWrap() {
   return btnWrap;
 }
 
-function createGithubBtn(btnWrap, isDarkmode) {
+function createGithubBtn(btnWrap, theme) {
   const githubBtn = document.createElement("a");
   githubBtn.href = "#";
   githubBtn.style.cursor = "pointer";
@@ -58,7 +58,7 @@ function createGithubBtn(btnWrap, isDarkmode) {
   };
 
   var githubURL = "icon/githubIcon.png";
-  if (isDarkmode) {
+  if (isDarkmode(theme)) {
     githubURL = "icon/githubDarkIcon.png";
   }
 
@@ -71,4 +71,67 @@ function createGithubBtn(btnWrap, isDarkmode) {
   githubBtn.append(githubImg);
 
   btnWrap.append(githubBtn);
+}
+
+function isDarkmode(theme) {
+  const themeMap = {
+    default: false,
+    "3024-day": false,
+    "3024-night": true,
+    abcdef: true,
+    ambiance: true,
+    "base16-dark": true,
+    "base16-light": false,
+    bespin: true,
+    blackboard: true,
+    cobalt: true,
+    colorforth: true,
+    darcula: true,
+    dracula: true,
+    "duotone-dark": true,
+    "duotone-light": false,
+    eclipse: false,
+    elegant: false,
+    "erlang-dark": true,
+    "gruvbox-dark": true,
+    hopscotch: true,
+    icecoder: true,
+    idea: false,
+    isotope: true,
+    "lesser-dark": true,
+    liquibyte: true,
+    lucario: true,
+    material: true,
+    mbo: true,
+    "mdn-like": false,
+    midnight: true,
+    monokai: true,
+    neat: false,
+    neo: false,
+    night: true,
+    nord: true,
+    "oceanic-next": true,
+    "panda-syntax": true,
+    "paraiso-dark": true,
+    "paraiso-light": false,
+    "pastel-on-dark": true,
+    railscasts: true,
+    rubyblue: true,
+    seti: true,
+    shadowfox: true,
+    "solarized dark": true,
+    "solarized light": false,
+    "the-matrix": true,
+    "tomorrow-night-bright": true,
+    "tomorrow-night-eighties": true,
+    ttcn: false,
+    twilight: true,
+    "vibrant-ink": true,
+    "xq-dark": true,
+    "xq-light": false,
+    yeti: false,
+    yonce: true,
+    zenburn: true,
+  };
+  return themeMap[theme] ?? false;
 }
