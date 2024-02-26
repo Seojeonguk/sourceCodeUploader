7986c917ba1899e62e653b7b7b07b0b17a5a3b9e
Seojeonguk <junguk7880@naver.com>
2024-02-26 15:38:47 +0900

    Show provide site list
    This will show provide site list.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- Plugin/popup.js
+++ Plugin/popup.js
@@ -1,37 +1,6 @@
 const providePlatporms = ["github", "notion"];
 
 $(function () {
-  const dropdowns = document.querySelectorAll(".dropdown");
-  dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener("click", (e) => {
-      e.stopPropagation();
-      dropdowns.forEach((c) => c.classList.remove("is-active"));
-      dropdown.classList.add("is-active");
-    });
-  });
-});
-
-$(document).click(function (e) {
-  var container = $(".status-button");
-  var dd = $(".dropdown");
-  if (!container.is(e.target) && container.has(e.target).length === 0) {
-    dd.removeClass("is-active");
-  }
-});
-
-$(function () {
-  $(".dropdown").on("click", function (e) {
-    $(".content-wrapper").addClass("overlay");
-    e.stopPropagation();
-  });
-  $(document).on("click", function (e) {
-    if ($(e.target).is(".dropdown") === false) {
-      $(".content-wrapper").removeClass("overlay");
-    }
-  });
-});
-
-$(function () {
   const toggleButton = document.querySelector(".dark-light");
 
   toggleButton.addEventListener("click", () => {
@@ -41,20 +10,19 @@ $(function () {
 
 $(function () {
   chrome.storage.local
-    .get(["repositories", "uploadedRepository"])
+    .get(["repositories", "githubUploadedRepository"])
     .then((res) => {
       const repositories = res?.repositories;
-      const uploadedRepository = res?.uploadedRepository;
+      const githubUploadedRepository = res?.githubUploadedRepository;
+
       if (repositories) {
-        $("#repository option").remove();
+        $("#repository-list li").remove();
         repositories.forEach((repository) => {
-          $("#repository").append(
-            `<option ${
-              repository.name === uploadedRepository ? "selected" : ""
-            }>${repository.name}</option>`
-          );
+          $("#repository-list").append(`<li><p>${repository.name}</p></li>`);
         });
       }
+
+      $("#selected-repository").text(githubUploadedRepository);
     });
 });
 
@@ -94,32 +62,43 @@ function sendMessageToBackground(className, action, payload) {
   });
 }
 
-$("#sync-repository").on("click", () => {
-  $("#repository option").remove();
-
-  chrome.runtime.sendMessage(
-    {
-      className: "github",
-      action: "getRepositories",
-    },
-    (repositories) => {
-      chrome.storage.local.set({ githubRepositories: repositories });
-      chrome.storage.local.get(["githubUploadedRepository"]).then((res) => {
-        const uploadedRepository = res?.githubUploadedRepository;
-
+$(function () {
+  $("#sync-repository").on("click", () => {
+    chrome.runtime.sendMessage(
+      {
+        className: "github",
+        action: "getRepositories",
+      },
+      (repositories) => {
+        $("#repository-list li").remove();
+        chrome.storage.local.set({ githubRepositories: repositories });
         repositories.forEach((repository) => {
-          $("#repository").append(
-            `<option ${
-              repository.name === uploadedRepository ? "selected" : ""
-            }>${repository.name}</option>`
-          );
+          $("#repository-list").append(`<li><p>${repository.name}</p></li>`);
         });
-      });
-    }
-  );
+      }
+    );
+  });
 });
 
-$("#save-repo").on("click", () => {
-  const value = $("#repositories").val();
-  chrome.storage.local.set({ githubUploadedRepository: value });
+$(function () {
+  $("#selected-repository").on("click", function (e) {
+    $("#repository-list").addClass("is-active");
+    $(".content-wrapper").addClass("overlay");
+    e.stopPropagation();
+  });
+
+  $(document).on("click", "#repository-list li p", function (e) {
+    const uploadedRepository = e.target.innerHTML;
+
+    chrome.storage.local
+      .set({ githubUploadedRepository: uploadedRepository })
+      .then(() => {
+        $("#selected-repository").text(uploadedRepository);
+      });
+  });
+
+  $(document).on("click", function (e) {
+    $("#repository-list").removeClass("is-active");
+    $(".content-wrapper").removeClass("overlay");
+  });
 });
