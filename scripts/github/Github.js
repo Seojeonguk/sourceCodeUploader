0b8e27cb95d2ec5666516d0fd3c8fbaa2cfa44b3
Seojeonguk <junguk7880@naver.com>
2024-02-28 14:59:53 +0900

    Manage the water supply separately (#109)
    This will manage the constants related to github in a separate file.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/github/Github.js
+++ scripts/github/Github.js
@@ -1,30 +1,22 @@
-const GITHUB_BASE_URL = "https://github.com";
-const GITHUB_API_BASE_URL = "https://api.github.com";
-const CLIENT_ID = "2cfd0f1fae095d5a1684";
-const CLIENT_SECRET = "d1b31e8c09708b856ecbe03d1f9e9223e472da89";
-const REDIRECT_URL = "https://github.com";
-const SCOPES = ["repo"];
-
-const OPEN_GITHUB_OAUTH_PAGE = "openGithubOauthPage";
+import * as Github from "./constants.js";
 
 async function dispatch(action, payload) {
-  if (action === OPEN_GITHUB_OAUTH_PAGE) {
+  if (action === Github.OPEN_OAUTH_PAGE) {
     openGithubOauthPage();
   }
 }
 
 function openGithubOauthPage() {
-  let parameters = `client_id=${CLIENT_ID}`;
-  if (REDIRECT_URL) {
-    parameters += `&redirect_url=${REDIRECT_URL}`;
+  let parameters = `client_id=${Github.CLIENT_ID}`;
+  if (Github.REDIRECT_URL) {
+    parameters += `&redirect_url=${Github.REDIRECT_URL}`;
   }
-  if (SCOPES) {
-    parameters += `&scope=${SCOPES[0]}`;
+  if (Github.SCOPES) {
+    parameters += `&scope=${Github.SCOPES[0]}`;
   }
-  const url = `${GITHUB_BASE_URL}/login/oauth/authorize?${parameters}`;
+  const url = `${Github.BASE_URL}/login/oauth/authorize?${parameters}`;
 
   chrome.tabs.create({ url: url });
 }
 
 export { dispatch };
-export { OPEN_GITHUB_OAUTH_PAGE };
---
+++ scripts/github/constants.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://github.com";
+const API_BASE_URL = "https://api.github.com";
+const CLIENT_ID = "2cfd0f1fae095d5a1684";
+const CLIENT_SECRET = "d1b31e8c09708b856ecbe03d1f9e9223e472da89";
+const REDIRECT_URL = "https://github.com";
+const SCOPES = ["repo"];
+
+const OPEN_OAUTH_PAGE = "openGithubOauthPage";
+
+export {
+  BASE_URL,
+  API_BASE_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL,
+  SCOPES,
+  OPEN_OAUTH_PAGE,
+};
