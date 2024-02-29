29f62fd160f9adda1f5c68bcba0e56c469d72e34
Seojeonguk <junguk7880@naver.com>
2024-02-29 14:44:46 +0900

    Add comments in buttonEvents file (#107)
    This will add comments in buttonEvents file.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- buttonEvents.js
+++ buttonEvents.js
@@ -6,6 +6,9 @@ function initializeButtonEvents() {
   syncRepository();
 }
 
+/**
+ * Closes the select list when clicking outside of it.
+ */
 function closeSelectListOnOutsideClick() {
   $(document).on("click", function () {
     $(".select-list ul").removeClass("is-active");
@@ -13,6 +16,11 @@ function closeSelectListOnOutsideClick() {
   });
 }
 
+/**
+ * Toggles dark mode when the dark-light element is clicked.
+ * Adds or removes the "light-mode" class from the body element based on its current state.
+ * Stops event propagation to prevent further event handling.
+ */
 function toggleDarkMode() {
   $(".dark-light").on("click", function (e) {
     $("body").toggleClass("light-mode");
@@ -20,6 +28,11 @@ function toggleDarkMode() {
   });
 }
 
+/**
+ * Shows the select list when the paragraph element inside select-list is clicked.
+ * Adds the "is-active" class to the select list ul and the "overlay" class to the content wrapper.
+ * Stops event propagation to prevent further event handling.
+ */
 function showSelectList() {
   $(".select-list p").on("click", function (e) {
     $(".select-list ul").addClass("is-active");
@@ -28,13 +41,18 @@ function showSelectList() {
   });
 }
 
+/**
+ * Handles click event on the authentication button.
+ * If the button has "delete" class, removes access token from Chrome storage and updates UI accordingly.
+ * Otherwise, sends a message to open GitHub OAuth page.
+ */
 function handleAuthenticationButton() {
   $(".authentication-btn").on("click", function () {
     if ($(this).hasClass("delete")) {
       const platform = $(this).closest("li").attr("platform");
       removeChromeStorage(`${platform}AccessToken`);
       $(this).removeClass("delete");
-      $(this).html("Authroize");
+      $(this).html("Authorize");
       $(this).closest("li").find(".status").removeClass("green");
     } else {
       sendMessage("github", "openGithubOauthPage");
@@ -42,9 +60,13 @@ function handleAuthenticationButton() {
   });
 }
 
+/**
+ * Handles click event on the sync repository button.
+ * Sends a message to background script to get authenticated user repositories for GitHub.
+ * Updates the repository list UI and stores the repositories in Chrome storage.
+ */
 function syncRepository() {
   $("#sync-repository").on("click", function (e) {
-    console.log("sync button click!");
     chrome.runtime.sendMessage(
       {
         platform: "github",
