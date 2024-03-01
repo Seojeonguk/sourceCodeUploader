f9910399b1dc9e82116832648ff3395aa294bfc5
Seojeonguk <junguk7880@naver.com>
2024-03-01 22:15:25 +0900

    Organize constant file (#111)
    This will add an export before the variable name and sort it.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/github/constants.js
+++ scripts/github/constants.js
@@ -1,24 +1,12 @@
-const BASE_URL = "https://github.com";
-const API_BASE_URL = "https://api.github.com";
-const CLIENT_ID = "2cfd0f1fae095d5a1684";
-const CLIENT_SECRET = "d1b31e8c09708b856ecbe03d1f9e9223e472da89";
-const REDIRECT_URL = "https://github.com";
-const SCOPES = ["repo"];
+export const BASE_URL = "https://github.com";
+export const API_BASE_URL = "https://api.github.com";
+export const CLIENT_ID = "2cfd0f1fae095d5a1684";
+export const CLIENT_SECRET = "d1b31e8c09708b856ecbe03d1f9e9223e472da89";
+export const REDIRECT_URL = "https://github.com";
+export const SCOPES = ["repo"];
 
-const OPEN_OAUTH_PAGE = "openGithubOauthPage";
-const REQUEST_AND_SAVE_ACCESS_TOKEN = "requestAndSaveAccessToken";
-const GET_AUTHENTICATED_USER_REPOSITORIES = "getAuthenticatedUserRepositories";
-const COMMIT = "commit";
-
-export {
-  BASE_URL,
-  API_BASE_URL,
-  CLIENT_ID,
-  CLIENT_SECRET,
-  COMMIT,
-  REDIRECT_URL,
-  SCOPES,
-  OPEN_OAUTH_PAGE,
-  REQUEST_AND_SAVE_ACCESS_TOKEN,
-  GET_AUTHENTICATED_USER_REPOSITORIES,
-};
+export const COMMIT = "commit";
+export const GET_AUTHENTICATED_USER_REPOSITORIES =
+  "getAuthenticatedUserRepositories";
+export const OPEN_OAUTH_PAGE = "openGithubOauthPage";
+export const REQUEST_AND_SAVE_ACCESS_TOKEN = "requestAndSaveAccessToken";
