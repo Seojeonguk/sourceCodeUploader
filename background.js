41e327e193b1f713504febe1768f751593cc279f
Seojeonguk <junguk7880@naver.com>
2024-03-19 00:02:19 +0900

    Introduce notion (#126)
    This will introduce notion.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
 import { dispatch as githubDispatch } from "./scripts/github/Github.js";
+import { dispatch as notionDispatch } from "./scripts/notion/Notion.js";
 
 function handleMessage(request, sender, sendResponse) {
   try {
     const { platform, action, payload } = request;
     if (platform === "github") {
       githubDispatch(action, payload)
+        .then((res) => {
+          sendResponse(res);
+        })
+        .catch((e) => {
+          sendResponse({ ok: false, message: e.message });
+        });
+    } else if (platform === "notion") {
+      notionDispatch(action, payload)
         .then((res) => {
           sendResponse(res);
         })
---
+++ scripts/notion/Notion.js
@@ -0,0 +1,11 @@
+import * as Util from "../util.js";
+
+async function dispatch(action, payload) {
+  try {
+    // To do more..
+  } catch (e) {
+    throw e;
+  }
+}
+
+export { dispatch };
