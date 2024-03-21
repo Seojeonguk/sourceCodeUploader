e9e0395b0dfaa62df413154b4f6a75c87ce533ec
Seojeonguk <junguk7880@naver.com>
2024-03-21 16:08:35 +0900

    Add comment (#128)
    This will add comment.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/boj/Boj.js
+++ scripts/boj/Boj.js
@@ -1,4 +1,8 @@
 let util;
+/**
+ * Load the util function dynamically.
+ * Attach the icon according to the code theme.
+ */
 (async () => {
   const src = chrome.runtime.getURL("scripts/util.js");
   util = await import(src);
