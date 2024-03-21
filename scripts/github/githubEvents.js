8e90818497ad1e6d2fb0b55373b12163b0423f12
Seojeonguk <junguk7880@naver.com>
2024-03-21 15:33:02 +0900

    Get notion access token (#126)
    This will get notion access token.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/github/githubEvents.js
+++ scripts/github/githubEvents.js
@@ -2,11 +2,10 @@ const currentUri = window.location.href;
 const code = currentUri.match(/code=([^&]*)/);
 if (code) {
   const payload = { code: code[1] };
+
   chrome.runtime.sendMessage({
-    platform: "github",
+    platform: currentUri.includes("state") ? "notion" : "github",
     action: "requestAndSaveAccessToken",
     payload,
   });
-
-  window.close();
 }
--- scripts/notion/Notion.js
+++ scripts/notion/Notion.js
@@ -1,9 +1,12 @@
 import * as Notion from "./constants.js";
+import * as Util from "../util.js";
 
 async function dispatch(action, payload) {
   try {
     if (action === Notion.OPEN_OAUTH_PAGE) {
       openOauthPage();
+    } else if (action === Notion.REQUEST_AND_SAVE_ACCESS_TOKEN) {
+      const accessToken = await requestAndSaveAccessToken(payload);
     }
   } catch (e) {
     throw e;
@@ -19,6 +22,59 @@ function openOauthPage() {
   const url = `${Notion.API_BASE_URL}/v1/oauth/authorize?${parameters}`;
 
   chrome.tabs.create({ url: url });
+}
+
+async function requestAndSaveAccessToken(payload) {
+  if (Util.isEmpty(payload)) {
+    throw new Error("Invalid payload object for reqeusting access token.");
+  }
+
+  const { code } = payload;
+  if (Util.isEmpty(code)) {
+    throw new Error("Invalid code for requesting access token.");
+  }
+
+  const url = `${Notion.API_BASE_URL}/v1/oauth/token`;
+
+  const data = JSON.stringify({
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: "https://github.com",
+  });
+
+  const encoded = btoa(`${Notion.CLIENT_ID}:${Notion.CLIENT_SECRET}`);
+
+  const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Basic ${encoded}`,
+  };
+
+  const response = await Util.request(url, "POST", headers, data);
+  if (!response.ok) {
+    throw new Error("Failed to fetch access token.");
+  }
+
+  const json = await response.json();
+
+  console.log(json);
+  const accessToken = json.access_token;
+  if (Util.isEmpty(accessToken)) {
+    throw new Error("Access token not found.");
+  }
+
+  const botId = json.bot_id;
+  const workspaceId = json.workspace_id;
+  const workspaceName = json.workspace_name;
+
+  Util.setChromeStorage("notionAccessToken", accessToken);
+  Util.setChromeStorage("notionBotId", botId);
+  Util.setChromeStorage("notionWorkspaceId", workspaceId);
+  Util.setChromeStorage("notionWorkspaceName", workspaceName);
+
+  Util.closeLatestTab();
+
+  return accessToken;
 }
 
 export { dispatch };
