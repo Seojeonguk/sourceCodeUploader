023c073b03a0ba4b52233a21aede6c22355579f4
Seojeonguk <junguk7880@naver.com>
2024-03-21 15:23:54 +0900

    Open notion oauth page (#126)
    This will open notion oauth page when notion authentication button click.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/notion/Notion.js
+++ scripts/notion/Notion.js
@@ -1,11 +1,24 @@
-import * as Util from "../util.js";
+import * as Notion from "./constants.js";
 
 async function dispatch(action, payload) {
   try {
-    // To do more..
+    if (action === Notion.OPEN_OAUTH_PAGE) {
+      openOauthPage();
+    }
   } catch (e) {
     throw e;
   }
+}
+
+function openOauthPage() {
+  let parameters = `owner=user&client_id=${Notion.CLIENT_ID}&response_type=code`;
+  if (Notion.REDIRECT_URL) {
+    parameters += `&redirect_uri=${Notion.REDIRECT_URL}`;
+  }
+
+  const url = `${Notion.API_BASE_URL}/v1/oauth/authorize?${parameters}`;
+
+  chrome.tabs.create({ url: url });
 }
 
 export { dispatch };
---
+++ scripts/notion/constants.js
@@ -0,0 +1,7 @@
+export const CLIENT_ID = "b5800b4c-70d0-4838-ab55-c7067b51354e";
+export const CLIENT_SECRET =
+  "secret_uHXz1yQjYMGS5j4fhGl3aIeY88AouLzAwp8CN7hs1DO";
+export const API_BASE_URL = "https://api.notion.com";
+export const REDIRECT_URL = "https://github.com";
+
+export const OPEN_OAUTH_PAGE = "openOauthPage";
