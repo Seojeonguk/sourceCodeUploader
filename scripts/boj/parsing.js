ef01f00f0c3fa1f6ddcb0f213f3dc09756eabab0
Seojeonguk <junguk7880@naver.com>
2024-03-21 16:22:57 +0900

    Update comment (#128)
    This will update comment.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/boj/parsing.js
+++ scripts/boj/parsing.js
@@ -1,5 +1,5 @@
 /**
- * Extracts the file extension based on the language obtained from the result table.
+ * Get the file extension to the language parsing the result table.
  *
  * @throws {Error} If the result table is not found or the language is not supported.
  * @returns {string} The file extension corresponding to the language.
@@ -20,7 +20,7 @@ function parsingExtension() {
 }
 
 /**
- * Parses the source code from a textarea element.
+ * Parse the source code from a textarea element.
  *
  * @returns {string} The parsed source code.
  * @throws {Error} If the textarea element with the name 'source' is not found.
@@ -44,8 +44,8 @@ function parsingTitle() {
 }
 
 /**
- * Parses the problem ID from the first anchor tag within a table that has a href attribute starting with '/problem/'.
- * If no anchor tag is found, it throws an error.
+ * Parse the problem ID from result table.
+ *
  * @returns {string} The problem ID parsed from the anchor tag.
  * @throws {Error} If no anchor tag with a href attribute starting with '/problem/' is found.
  */
@@ -59,7 +59,8 @@ function parsingProblemID() {
 }
 
 /**
- * Parses the CodeMirror theme being used in the application.
+ * Parse the CodeMirror theme from CodeMirror.
+ *
  * @returns {string} The name of the CodeMirror theme.
  * @throws {Error} Throws an error if CodeMirror element is not found.
  */
