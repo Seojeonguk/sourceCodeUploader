fda140c3d8381c14594f7b6bd9b2573cebc1a00e
Seojeonguk <junguk7880@naver.com>
2024-03-28 01:06:14 +0900

    Delete the key of the page object (#142)
    This will delete the key of the page object.
    
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/notion/objects/page.js
+++ scripts/notion/objects/page.js
@@ -3,12 +3,11 @@ import { createRichTextArray } from "./block.js";
 
 /**
  * Create a page property.
- * @param {Object} param - Object containing the type and payload for creating the page property
- * @param {string} param.type - The type of page property to create
- * @param {Object} param.payload - The payload containing the necessary data for creating the page property
+ * @param {string} type - The type of page property to create
+ * @param {Object} payload - The payload containing the necessary data for creating the page property
  * @returns {Object} - The created page property object
  */
-export const createPageProperty = ({ type, payload }) => {
+export const createPageProperty = (type, payload) => {
   switch (type) {
     case PROPERTY_TYPE.DATE:
       return createDate(payload);
@@ -28,18 +27,15 @@ export const createPageProperty = ({ type, payload }) => {
 /**
  * Create a date property.
  * @param {Object} param - Object containing the parameters for creating the date property
- * @param {string} param.key - The key name of the date property
  * @param {Date} [param.start=new Date()] - The start date for the date property, defaults to the current date if not provided
  * @param {Date|null} [param.end=null] - The end date for the date property, defaults to null if not provided
  * @returns {Object} - The created date property object
  */
-export const createDate = ({ key, start = new Date(), end = null }) => {
+export const createDate = ({ start = new Date(), end = null }) => {
   return {
-    [key]: {
-      [PROPERTY_TYPE.DATE]: {
-        start,
-        end,
-      },
+    [PROPERTY_TYPE.DATE]: {
+      start,
+      end,
     },
   };
 };
@@ -47,49 +43,42 @@ export const createDate = ({ key, start = new Date(), end = null }) => {
 /**
  * Create a multi-select property.
  * @param {Object} params - Object containing the parameters for creating the multi-select property
- * @param {string} params.key - The key name of the multi-select property
  * @param {Array} [params.selections=[]] - An array of objects representing the selections for the multi-select property
  * @param {string} [params.selections.id=null] - The ID of the selection (optional)
  * @param {string} params.selections.name - The name of the selection
  * @param {string} [params.selections.color=default] - The color of the selection, default is SELECT_COLOR.DEFAULT
  * @returns {Object} - The created multi-select property object
  */
-export const createMultiSelect = ({ key, selections = [] }) => {
+export const createMultiSelect = ({ selections = [] }) => {
   return {
-    [key]: {
-      [PROPERTY_TYPE.MULTI_SELECT]: selections.map(
-        ({ color = SELECT_COLOR.DEFAULT, id = null, name }) => ({
-          id,
-          name,
-          color,
-        })
-      ),
-    },
+    [PROPERTY_TYPE.MULTI_SELECT]: selections.map(
+      ({ color = SELECT_COLOR.DEFAULT, id = null, name }) => ({
+        id,
+        name,
+        color,
+      })
+    ),
   };
 };
 
 /**
  * Create a select property.
  * @param {Object} params - Object containing the parameters for creating the select property
- * @param {string} params.key - The key for the select property
  * @param {string} [params.color=SELECT_COLOR.DEFAULT] - The color of the select option, default is SELECT_COLOR.DEFAULT
  * @param {string} [params.id=null] - The ID of the select option (optional)
  * @param {string} params.name - The name of the select option
  * @returns {Object} - The created select property object
  */
 export const createSelect = ({
-  key,
   color = SELECT_COLOR.DEFAULT,
   id = null,
   name,
 }) => {
   return {
-    [key]: {
-      [PROPERTY_TYPE.SELECT]: {
-        color,
-        name,
-        id,
-      },
+    [PROPERTY_TYPE.SELECT]: {
+      color,
+      name,
+      id,
     },
   };
 };
@@ -97,31 +86,25 @@ export const createSelect = ({
 /**
  * Create a title property.
  * @param {Object} params - Object containing the parameters for creating the title property
- * @param {string} params.key - The key for the title property
  * @param {string} params.text - The text content of the title
  * @returns {Object} - The created title property object
  */
-export const createTitle = ({ key, text }) => {
+export const createTitle = ({ text }) => {
   return {
-    [key]: {
-      id: PROPERTY_TYPE.TITLE,
-      type: PROPERTY_TYPE.TITLE,
-      title: createRichTextArray(text),
-    },
+    id: PROPERTY_TYPE.TITLE,
+    type: PROPERTY_TYPE.TITLE,
+    title: createRichTextArray(text),
   };
 };
 
 /**
  * Create a URL property.
  * @param {Object} params - Object containing the parameters for creating the URL property
- * @param {string} params.key - The key for the URL property
  * @param {string} params.url - The URL value for the property
  * @returns {Object} - The created URL property object
  */
-export const createUrl = ({ key, url }) => {
+export const createUrl = ({ url }) => {
   return {
-    [key]: {
-      url,
-    },
+    url,
   };
 };
