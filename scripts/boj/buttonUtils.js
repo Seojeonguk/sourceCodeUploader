a8505999371749cc13f47403e630366714ea2b96
Seojeonguk <junguk7880@naver.com>
2024-09-02 00:33:19 +0900

    Add createButton comment. (#155)
    Signed-off-by: Seojeonguk <junguk7880@naver.com>

--- scripts/boj/buttonUtils.js
+++ scripts/boj/buttonUtils.js
@@ -9,6 +9,14 @@ function createButtonWrapper() {
   return div;
 }
 
+/**
+ * Creates a button element with an image and attaches a click event listener.
+ *
+ * @param {string} imageUrl - The URL or path to the image that will be displayed on the button.
+ * @param {function} clickEvent - The event handler function to be called when the button is clicked.
+ * @returns {HTMLElement} - The created button element containing the image and attached click event.
+ *
+ */
 const createButton = (imageUrl, clickEvent) => {
   const img = document.createElement("img");
   img.src = util.getResourceURL(imageUrl);
